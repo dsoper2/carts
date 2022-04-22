1143a712ca7cb8e14820cc9f96933cdb392ffb26
“dsoper2” <dsoper@cisco.com>
2022-04-22 14:51:05 -0500

    Fix build warnings

--- src/test/java/works/weave/socks/cart/controllers/UnitHealthCheckController.java
+++ src/test/java/works/weave/socks/cart/controllers/UnitHealthCheckController.java
@@ -15,7 +15,7 @@ import static org.mockito.Mockito.*;
 import works.weave.socks.cart.entities.HealthCheck;
 
 
-import java.util.ArrayList;
+# import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
